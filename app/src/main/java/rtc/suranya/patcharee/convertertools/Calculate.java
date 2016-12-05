5737377c9f509d5ae8666c84d935f59a9f4e7163
masterUNG <phrombutr@gmail.com>
2016-12-05 15:35:25 +0700

    Intent OK

--- app/src/main/java/rtc/suranya/patcharee/convertertools/Calculate.java
+++ app/src/main/java/rtc/suranya/patcharee/convertertools/Calculate.java
@@ -2,12 +2,21 @@ package rtc.suranya.patcharee.convertertools;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class Calculate extends AppCompatActivity {
+
+    //Explicit
+    private int indexCatAnInt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculate);
-    }
-}
+
+        indexCatAnInt = getIntent().getIntExtra("Cat", 0);
+        Log.d("5decV1", "indexCat ==> " + indexCatAnInt);
+
+    }   // Main Method
+
+}   // Main Class
