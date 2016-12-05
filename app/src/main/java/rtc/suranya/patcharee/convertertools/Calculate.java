30007b20b1818087be7dc870456b2f215d961aff
masterUNG <phrombutr@gmail.com>
2016-12-05 15:46:32 +0700

    Near OK

--- app/src/main/java/rtc/suranya/patcharee/convertertools/Calculate.java
+++ app/src/main/java/rtc/suranya/patcharee/convertertools/Calculate.java
@@ -3,11 +3,20 @@ package rtc.suranya.patcharee.convertertools;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Calculate extends AppCompatActivity {
 
     //Explicit
     private int indexCatAnInt;
+    private EditText editText;
+    private Spinner topSpinner, backSpinner;
+    private TextView textView;
+    private String string;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +26,25 @@ public class Calculate extends AppCompatActivity {
         indexCatAnInt = getIntent().getIntExtra("Cat", 0);
         Log.d("5decV1", "indexCat ==> " + indexCatAnInt);
 
+        //Bind Widget
+        editText = (EditText) findViewById(R.id.editText);
+        topSpinner = (Spinner) findViewById(R.id.spinner);
+        backSpinner = (Spinner) findViewById(R.id.spinner2);
+        textView = (TextView) findViewById(R.id.textView2);
+
     }   // Main Method
+
+    public void clickCalculate(View view) {
+
+        string = editText.getText().toString().trim();
+
+        if (string.equals("")) {
+            //Have Space
+            Toast.makeText(Calculate.this, "โปรดกรอง ค่าที่ต้องการจะแปลง", Toast.LENGTH_SHORT).show();
+        } else {
+            //No Space
+        }
+
+    }
 
 }   // Main Class
